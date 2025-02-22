5dec296dcb66cd16c49cdbb785e4ad562a8feb51
sullivaneg <emma.sullivan@mymail.champlain.edu>
2025-02-22 00:31:01 -0500

    secure ssh

---
+++ linux/ubuntu/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author sullivaneg
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in (for Centos7, no neccesary for Ubuntu)
+echo "ALL YOUR CODE GOES HERE"
